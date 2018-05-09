1d4d08fe3e671365900caa4980f7cb679357cdb5
Raulkg <gaddam.rahul.kumar@gmail.com>
2018-05-08 20:39:27 -0400

    Data Access Object

---
+++ services/src/main/java/com/skool/Dao/StudentDao.java
@@ -0,0 +1,19 @@
+package com.skool.Dao;
+
+public class StudentDao {
+    private final long id;
+    private final String content;
+
+    public StudentDao(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
